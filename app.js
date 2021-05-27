473f291d47d8329420b14ab4b52db909fe6899bd
AnantaGur <grgananta99@gmail.com>
2021-05-27 12:46:49 -0400

    new array finder

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 function computerPlay() {
-
-    let random = Math.floor(Math.random() * 3 + 1);
-
-    if (random === 1) {
-        console.log('Rock');
-    } else if (random === 2) {
-        console.log('Paper');
-    } else if (random === 3) {
-        console.log('Scissors');
-    }
-};
-
-
-
+    choices = ['Rock', 'Paper', 'Scissors']
+    let random = choices[Math.floor(Math.random() * choices.length)]
+    return random;
+}
