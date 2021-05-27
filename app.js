21d16a69c84ec0ee5380b9cec47520bf4895add6
AnantaGur <grgananta99@gmail.com>
2021-05-27 13:09:28 -0400

    Winner and loser, computerPlay function complete

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ function computerPlay() {
     let random = choices[Math.floor(Math.random() * choices.length)]
     return random;
 }
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay()
+
+    if (playerSelection == computerSelection) {
+        return ('This is a tie');
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+        return ('Congrats you win!');
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+        return ('Too bad you lose!');
+    } else {
+        console.log('Something Went wrong')
+    }
+}
+
