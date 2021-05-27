08bf80c0ae74355e41ee66b917f97c78c280a1ed
AnantaGur <grgananta99@gmail.com>
2021-05-27 14:11:34 -0400

    Made everything case insensitive

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+
 function computerPlay() {
-    choices = ['Rock', 'Paper', 'Scissors']
+    choices = ['rock', 'paper', 'scissors']
     let random = choices[Math.floor(Math.random() * choices.length)]
     return random;
 }
 
 function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay()
+    computerSelection = computerPlay().toLowerCase()
+    playerSelection = playerSelection.toLowerCase()
 
     if (playerSelection == computerSelection) {
         return ('This is a tie');
-    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
-        (playerSelection == 'Paper' && computerSelection == 'Rock') ||
-        (playerSelection == 'Scissors' && computerSelection == 'Paper')) {
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')) {
         return ('Congrats you win!');
-    } else if ((playerSelection == 'Rock' && computerSelection == 'Paper') ||
-        (playerSelection == 'Paper' && computerSelection == 'Scissors') ||
-        (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+    } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'rock')) {
         return ('Too bad you lose!');
+        //could've also done }else {console.log('You lose!')} instead of else if and recreating every scenario.
     } else {
         console.log('Something Went wrong')
     }
+
 }
 
