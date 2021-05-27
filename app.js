5613c0a8f839e85137faf227435adf201be98377
AnantaGur <grgananta99@gmail.com>
2021-05-27 14:52:49 -0400

    Scoring and ending added extra prompts still needed loops will be next lesson

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-
 function computerPlay() {
     choices = ['rock', 'paper', 'scissors']
     let random = choices[Math.floor(Math.random() * choices.length)]
     return random;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay().toLowerCase()
     playerSelection = playerSelection.toLowerCase()
 
     if (playerSelection == computerSelection) {
-        return ('This is a tie');
+        return (`This is a tie the score is still ${playerScore} : ${computerScore}`);
     } else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
         (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        return ('Congrats you win!');
+        ++playerScore
+        if (playerScore == 5) {
+            return ('You have won it all baby!');
+        } return (`You win you\'re at ${playerScore} : ${computerScore}`)
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'scissors') ||
         (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        return ('Too bad you lose!');
+        ++computerScore
+        if (computerScore == 5) {
+            return ('Too bad you lose!');
+        } return (`You lose you\'re at ${playerScore} : ${computerScore}`)
         //could've also done }else {console.log('You lose!')} instead of else if and recreating every scenario.
     } else {
         console.log('Something Went wrong')
     }
-
 }
+
 
